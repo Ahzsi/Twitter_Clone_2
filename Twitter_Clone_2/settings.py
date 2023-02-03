8aac0e379a7f36e70da9a117e65930fd63bc6716
Ahzsi <zclee95@gmail.com>
2023-02-02 21:52:35 -0600

    Connected app to main project. Deleted __pycache__. Added app to settings file. created urls.py for app.

--- Twitter_Clone_2/settings.py
+++ Twitter_Clone_2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Twitter_Posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
