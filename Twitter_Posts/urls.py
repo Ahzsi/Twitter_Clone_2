7a930a967ea8597c305428470a045632195677d4
Ahzsi <zclee95@gmail.com>
2023-02-08 22:17:43 -0600

    Updated edit page. Connected pages to homepage

--- Twitter_Posts/urls.py
+++ Twitter_Posts/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('delete/<int:post_ID>/', views.delete, name='delete'),
+    path('edit/<int:post_ID>/', views.edit, name='edit'),
+    path('like/<int:post_ID>', views.LikePost, name='like')
 ]
